d1f9caa698caa19f2211b758165b2c0ae8c5aeee
MadhuSudan <madhusudan74748@outlook.com>
2022-04-26 12:34:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AsteroidGame
+ This is a game that I have created with python from the tutorial provided by realpython.
---
+++ space_rocks/__main__.py
@@ -0,0 +1,5 @@
+from game import SpaceRocks
+
+if __name__ == "__main__":
+    Space_Rocks = SpaceRocks()
+    Space_Rocks.main_loop()
